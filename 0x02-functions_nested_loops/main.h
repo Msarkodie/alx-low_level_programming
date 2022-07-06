ba41b099f22db612a90d5a051cbb783061354068
Michael Sarkodie <sarkodiemichael399@gmail.com>
2022-07-06 03:21:29 +0000

    nest

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -30,3 +30,53 @@ int _islower(int c);
  * checks for alphabetic character
  */
 int _isalpha(int c);
+
+/*
+ * prints the sign of a number.
+ */
+int print_sign(int n);
+
+/**
+ *  * print_last_digit - print last digit of integer
+ *   * @int: integer
+ *    * Return: last digit of number
+ *     */
+
+int print_last_digit(int);
+
+/**
+ *  * jack_bauer - print every minute of a day
+ *   * Description: print minutes 00:00 to 23:59
+ *    */
+
+void jack_bauer(void);
+
+
+/**
+ *  * times_table - print multiplication table
+ *   */
+
+void times_table(void);
+
+/**
+ *  * add - add two integers
+ *   * @a: first integer
+ *    * @b: second integer
+ *     * Return: sum
+ *      */
+
+int add(int a, int b);
+
+/**
+ *  * print_to_98 - print numbers n to 98
+ *   * @n: integer argument
+ *    */
+
+void print_to_98(int n);
+
+/**
+ *  * print_times_table - print multiplication table up to n
+ *   * @n: integer argument
+ *    */
+
+void print_times_table(int n);
