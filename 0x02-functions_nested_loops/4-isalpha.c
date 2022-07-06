339ba832645d84316277a0f66653eda466636a98
Michael Sarkodie <sarkodiemichael399@gmail.com>
2022-07-06 02:59:26 +0000

    hmm

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,18 +3,18 @@
 /**
  *  * _isalpha - checks for alphabet character
  *   * @c: character to check
- *    * Return: 1 if lower or uppercase, 0 if not alphabet character
+ *  Return: 1 if lower or uppercase, 0 if not alphabet character
  */
 
 int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 	{
-		if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-		{
-			return (1);
-		}
-		else
-		{
-			return (0);
-		}
-		_putchar('\n');
+		return (1);
 	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
