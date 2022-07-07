dae7d55c20e5c280f272d4e290b0de39f16e1bb9
Michael Sarkodie <sarkodiemichael399@gmail.com>
2022-07-07 12:54:00 +0000

    Adding the main header

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,51 @@
+/**
+ * checks upper case character
+ */
+int _isupper(int c);
+
+
+/**
+* checks for a digit(from 0 through 9
+*/
+int _isdigit(int c);
+
+/**
+ * multiplies two integers
+ */
+int mul(int a, int b);
+
+
+/**
+ * prints the numbers from 0 through 9 followed by a new line
+ */
+void print_numbers(void);
+
+
+/**
+ * prints 10 times the numbers from 0 to 14
+ */
+void more_numbers(void);
+
+/**
+ * draws a straight line in the termonal
+ */
+void print_line(int n);
+
+/**
+ * draws a diagonal line in the terminal
+ */
+void print_diagonal(int n);
+
+/**
+ * prints a square follwed by a new line
+ */
+void print_square(int size);
+
+
+/**
+ * prints a triangle followed by a new line
+ */
+void print_triangle(int size);
+
+
+
