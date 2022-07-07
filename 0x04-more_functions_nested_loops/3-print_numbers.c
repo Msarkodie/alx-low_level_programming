7c7125260abab09f5b42ee23fd735705e7235ab7
Michael Sarkodie <sarkodiemichael399@gmail.com>
2022-07-07 15:13:03 +0000

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers- print numbers from 0 to 9
+ * Return: Returns nothing
+ */
+
+void print_numbers(void)
+{
+	int counter;
+
+	for (counter = 0; counter <= 9; counter++)
+	{
+		_putchar(counter);
+	}
