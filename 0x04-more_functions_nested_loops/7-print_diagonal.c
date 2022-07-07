adf6fcbc1d53d7354808e865e37332578132d06b
Michael Sarkodie <sarkodiemichael399@gmail.com>
2022-07-07 18:38:51 +0000

    diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *  * print_diagonal- prints a straight line
+ *   * @n: The number of times the char should be printed
+ */
+
+void print_diagonal(int n)
+{
+	int i;
+	int space;
+	int draw;
+
+	for (draw = 1; draw <= n; draw++)
+	{
+		for (space = 1; space < draw ; space++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+}
+_putchar('\n');
+}
