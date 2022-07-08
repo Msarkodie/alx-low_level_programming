90aece4332d8af7d5e0a8276e596a8735c37a74d
Michael Sarkodie <sarkodiemichael399@gmail.com>
2022-07-08 04:54:23 +0000

    hmm

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -6,11 +6,12 @@
  *    * @b: second integer
  *     * @c: third integer
  *      * Return: largest number
- *       */
+ */
 
 int largest_number(int a, int b, int c)
 {
 	int largest;
+
 	largest = a;
 
 	if (b > a)
