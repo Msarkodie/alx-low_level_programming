4c04e00de982315b433d466f5427a1a0b69620f4
Michael Sarkodie <sarkodiemichael399@gmail.com>
2022-07-08 02:14:59 +0000

    triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ *print_triangle- prints a triangle
+ *@size: is the size of triangle
+ *Return: 0 on success
+ */
+
+void print_triangle(int size)
+{
+	int i;
+	int space;
+
+	if (size > 0)
+	{
+
+		for (i = 1; i <= size; i++)
+		{
+			for (space = size - i; space > 0; space--)
+			{
+				_putchar(" ");
+			}
+			_putchar("#");
+		}
+	}
+	else
+	{
+		_putchar("\n");
+	}
+}
