15fb0275a10ba60ae30f0a33e4c09156699bb853
Michael Sarkodie <sarkodiemichael399@gmail.com>
2022-07-13 10:41:08 +0000

    Adding the strcat function

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,38 @@
+/*
+ * char *_strcat - Appends src to the dest string
+ */ 
+char *_strcat(char *dest, char *src);
+
+/*
+ * char *_strncat - concatenates strings with at most n bytes from src
+ */
+char *_strncat(char *dest, char *src, int n);
+
+/*
+ * char *_strncopy - copies a string
+ */
+char *_strncpy(char *dest, char *src, int n);
+
+/*
+ * compares two strings
+ */
+int _strcmp(char *s1, char *s2);
+
+/*
+ * reverses the content of an array of integers
+ */
+void reverse_array(int *a, int n);
+
+/*
+ * changes all lower case letters to upper case
+ */
+char *string_toupper(char *);
+
+/*
+ * capitalises all words of a string
+ */
+char *cap_string(char *);
+
+
+
+
