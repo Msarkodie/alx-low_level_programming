be3de5dc0c6a991f94773e746a36fe6d6ef82127
Michael Sarkodie <sarkodiemichael399@gmail.com>
2022-07-13 12:56:34 +0000

    The strncopy function

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * _strncpy - copies src unto dest based on n
+ * @dest: the buffer to be copied to
+ * @src: the string to copy
+ * @n: The determiner in the process
+ * Return: the pointer to the dest string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j = 0'
+
+	while(src[i] != '\0')
+	{
+		if (i < n)
+		{
+			dest[i] = src[i];
+		}
+		i++;
+	}
+
+	while (i < n && src[i] == '\0')
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
