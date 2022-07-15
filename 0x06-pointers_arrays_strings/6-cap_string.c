60ef32f85c6409e3ebe1cfca5f966ca6fbaee351
Michael Sarkodie <sarkodiemichael399@gmail.com>
2022-07-15 05:33:33 +0000

    caps

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * cap_string - capitalises all the chars
+ * @s: the string o]in question
+ * Return: the capitalised string
+ */
+
+
+char *cap_string(char *s)
+{
+	int i = 0;
+
+	if (s[i] >= 'a' && s[i] <= 'z')
+		s[i] = s[i] - 'a' + 'A';
+	i++;
+
+	while (s[i] != '\0') /* iterate through string */
+	{
+
+		if ((s[i] >= 'a' && s[i] <= 'z') &&
+		(s[i - 1] == ',' || s[i - 1] == ';' || s[i - 1] == '.' ||
+		s[i - 1] == '!' || s[i - 1] == '?' || s[i - 1] == '"' ||
+		s[i - 1] == '(' || s[i - 1] == ')' || s[i - 1] == '{' ||
+		s[i - 1] == '}' || s[i - 1] == ' ' || s[i - 1] == '\t'
+		|| s[i - 1] == '\n'))
+		s[i] = s[i] - 'a' + 'A';
+		i++;
+	}
+
+	return (s);
+}
