e2317bf931dff9bf0499184e3f64600c2e3b6271
Michael Sarkodie <sarkodiemichael399@gmail.com>
2022-07-21 05:32:32 +0000

    wildcmp yee

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -18,14 +18,14 @@ int wildcmp(char *s1, char *s2)
 			j++;
 		}
 
-	if (s2[j] == "*")
-	{
-		j++;
-		while ((s1[i] != s2[j]) && s1[i] != '\0')
-			i++;
-	}
-	if (s1[i] != s2[j])
-		return (0);
-	}
-	return (1);
+		if (s2[j] == "*")
+		{
+			j++;
+			while ((s1[i] != s2[j]) && s1[i] != '\0')
+				i++;
+		}
+		if (s1[i] != s2[j])
+			return (0);
+		}
+		return (1);
 }
