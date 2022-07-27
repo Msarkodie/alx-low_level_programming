006f1a5f9a1b5e49cc854fc42cc30feaae893e99
Michael Sarkodie <sarkodiemichael399@gmail.com>
2022-07-27 03:40:15 +0000

    strtow

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,106 +1,75 @@
-#include "main.h"
+#include "holberton.h"
 #include <stdlib.h>
-#include <stdio.h>
-
 /**
- * _strlen - computes for the length
- *   * @s: string
- *    * @i: number
- *     * Return: 0
- */
-int _strlen(char *s, int *i);
-
-/**
- * **strtow - length
- * @str: string
- * *Return: 0
- */
-char **strtow(char *str)
+ * * wordCounterRec - count num of words recursively
+ * * @str: pointer to char
+ * * @i: current index
+ * * Return: number of words
+ * **/
+int wordCounterRec(char *str, int i)
 {
-	int i, count = 0, len = 0, r = 0, copy = 0;
-	char **s;
-
-	if (str == NULL || str[0] == '\0')
-		return (NULL);
-
-	s = malloc(100 * sizeof(*s));
-	if (s == NULL)
-		return (NULL);
-	for (i = 0; str[i]; i++)
-	{
-		if (str[i] != 32)
-		{
-			copy = i;
-			len = _strlen(str, &i);
-			printf("%d\n", len);
-			s[count] = malloc(len * sizeof(*s));
-			if (s[count] == NULL)
-			{
-				free_grid(s, i);
-				return (NULL);
-																		}
-
-			for (r = 0; r < len; r++)
-			{
-				if (copy < i)
-																			{
-					s[count][r] = str[copy];
-					copy++;
-				}
-			}
-			count++;
-																	}
-		else
-		{
-			while (str[i] == 32 && str[i + 1] == 32)
-				i++;
-		}
-	}
-	return (s);
+	if (str[i] == '\0')
+		return (0);
+	if (str[i] == ' ' && str[i + 1] != ' ' && str[i + 1] != '\0')
+		return (1 + wordCounterRec(str, i + 1));
+	return (wordCounterRec(str, i + 1));
 }
 /**
- * _strlen- computes for the length
- * @str: string
- * @i: number
- * Return: 0
- */
-int _strlen(char *str, int *i)
+ * * word_counter - counts number of words in 1d array of strings
+ * * @str: pointer to char
+ * * Return: number of words
+ * **/
+int word_counter(char *str)
 {
-	int len = 0;
-	int c = 0;
+	if (str[0] != ' ')
+		return (1 + wordCounterRec(str, 0));
+	return (wordCounterRec(str, 0));
+}
+/**
+ * * strtow - splits a string into words.
+ * * @str: string to be splitted
+ * * Return: pointer to an array of strings (words) or null
+ * **/
+char **strtow(char *str)
+{
+	char **strDup;
+	int i, n, m, words;
 
-	c = *i;
-	while (str[c] != 32)
+	if (str == NULL || str[0] == 0)
+		return (NULL);
+	words = word_counter(str);
+	if (words < 1)
+		return (NULL);
+	strDup = malloc(sizeof(char *) * (words + 1));
+	if (strDup == NULL)
+		return (NULL);
+	i = 0;
+	while (i < words && *str != '\0')
 	{
-		if (str[c] > 64 && str[c] < 91)
+		if (*str != ' ')
 		{
-			if (len == 0 && (str[c + 1] > 96 && str[c + 1] < 123))
-				len++;
-			else if (len == 0 && (str[c + 1]  > 64 && str[c + 1] < 91))
-				len++;
-			else
-				len++;
-																	}
-		else if (str[c] > 96 && str[c] < 123)
-		{
-			if (len == 0 && (str[c + 1] > 96 && str[c + 1] < 123))
-				len++;
-			else if (len == 0 && (str[c + 1]  > 64 && str[c + 1] < 91))
-				len++;
-			else
-				len++;
-																	}
-		else if (str[c] == 35)
-		{
-			if (str[c + 1]  > 64 && str[c + 1] < 91)
-				len++;
-			else if (str[c + 1] > 96 && str[c + 1] < 123)
-				len++;
-																	}
-		else if (str[c] == 46 && str[c + 1] == 32)
-			len++;
-		c++;
-		*i = c;
+			n = 0;
+			while (str[n] != ' ')
+				n++;
+			strDup[i] = malloc(sizeof(char) * (n + 1));
+			if (strDup[i] == NULL)
+			{
+				while (--i >= 0)
+					free(strDup[--i]);
+				free(strDup);
+				return (NULL);
+			}
+			m = 0;
+			while (m < n)
+			{
+				strDup[i][m] = *str;
+				m++, str++;
+			}
+			strDup[i][m] = '\0';
+			i++;
+		}
+		str++;
 	}
-	return (len);
+	strDup[i] = '\0';
+	return (strDup);
 }
