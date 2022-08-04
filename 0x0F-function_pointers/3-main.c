be3fcaf2461cf5ec3f3177f11330e873866e0856
Michael Sarkodie <sarkodiemichael399@gmail.com>
2022-08-04 05:02:14 +0000

    bunches

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,56 +1,46 @@
-#include <stdio.h>
-#include "function_pointers.h"
+#include <stdio.h> /* printf */
+#include <stdlib.h> /* atoi */
+#include "3-calc.h"
 
 /**
- *  * is_98 - check if a number is equal to 98
- *   * @elem: the integer to check
- *    *
- *     * Return: 0 if false, something else otherwise.
+ *  * main - when user runs main,
+ *   * user should give two integers and an operator and
+ *    * main will calculate the math via a function pointer.
+ *     * prints sum, difference, product, dividend, or remainder
+ *      * @argc: argument counter
+ *       * @argv: arguments
+ *        * Return: 0 on sucess
  */
-int is_98(int elem)
+
+int main(int argc, char *argv[])
 {
-	return (elem == 98);
-}
+	int n1, n2;
+	int (*f)(int, int);
 
-/**
- *  * is_strictly_positive - check if a number is greater than 0
- *   * @elem: the integer to check
- *    *
- *     * Return: 0 if false, something else otherwise.
- */
-int is_strictly_positive(int elem)
-{
-	return (elem > 0);
-}
+	/* validate input */
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
 
+	/* convert user input to ints and point to correct operator function */
+	n1 = atoi(argv[1]);
+	n2 = atoi(argv[3]);
+	f = get_op_func(argv[2]);
 
-/**
- *  * abs_is_98 - check if the absolute value of a number is 98
- *   * @elem: the integer to check
- *    *
- *     * Return: 0 if false, something else otherwise.
- */
-int abs_is_98(int elem)
-{
-	return (elem == 98 || -elem == 98);
-}
+	if (f == NULL || (argv[2][1] != '\0'))
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && argv[3][0] == '0')
+	{
+		printf("Error\n");
+		exit(100);
+	}
 
-/**
- *  * main - check the code for Holberton School students.
- *   *
- *    * Return: Always 0.
- */
-int main(void)
-{
-	int array[20] = {0, -98, 98, 402, 1024, 4096, -1024, -98, 1, 2, 3,
-	4, 5, 6, 7, 8, 9, 10, 11, 98};
-	int index;
+	printf("%d\n", f(n1, n2)); /* calculate via function ptr */
 
-	index = int_index(array, 20, is_98);
-	printf("%d\n", index);
-	index = int_index(array, 20, abs_is_98);
-	printf("%d\n", index);
-	index = int_index(array, 20, is_strictly_positive);
-	printf("%d\n", index);
 	return (0);
 }
