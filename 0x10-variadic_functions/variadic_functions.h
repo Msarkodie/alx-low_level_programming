805d4270b5ad26f8209be23c35565878a1118a8d
Michael Sarkodie <sarkodiemichael399@gmail.com>
2022-08-05 05:17:34 +0000

    sum the all and header

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,24 @@
+#ifndef VARIADIC_FUNC
+#define VARIADIC_FUNC
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+
+#include <stdarg.h>
+/**
+ * * struct type - Struct for calling the right printf for printing anything
+ * * @a: format specifier for printf
+ * * @f: formatter
+ * * @ttype: pointer to function for appropriate printf()
+ */
+typedef struct type
+{
+	char a;
+	char *f;
+	void (*ttype)(char *, va_list);
+} type;
+
+void print_all(const char * const format, ...);
+
+#endif
