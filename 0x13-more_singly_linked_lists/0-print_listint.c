51ba08b5404954fc8188ecf62392af91579f18ee
Michael Sarkodie <sarkodiemichael399@gmail.com>
2022-08-17 08:09:02 +0000

    Adding the print_listint function

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_listint - prints all the elements in a linked list
+ * @h:The header of the linked list
+ * Return:The number of elements in the linked list
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	int count = 0;
+
+	if (h == NULL)
+	{
+		return (0);
+	}
+	while (h != NULL)
+	{
+		printf("%d \n", h->n);
+		h = h->next;
+		count++;
+	}
+
+	return (count);
+}
