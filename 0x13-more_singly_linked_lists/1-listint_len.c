6409c82046041303180aa9b215c2ef6e37118045
Michael Sarkodie <sarkodiemichael399@gmail.com>
2022-08-17 10:14:35 +0000

    added the listint_len file

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,21 @@
+#include "list.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * listint_len - calculate the number of nodes in the list
+ * @h:The linked list head
+ * Return:The number of list
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	int count = 0;
+
+	while (h != NULL)
+	{
+		printf("%d \n", h->n);
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
