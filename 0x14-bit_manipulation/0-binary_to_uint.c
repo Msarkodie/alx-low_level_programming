6491927a20ef07ca8afd9dd0897c9ff211c5b6ec
Michael Sarkodie <sarkodiemichael399@gmail.com>
2022-08-31 11:39:26 +0000

    Adding the main header and the binary to uint files

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+unsigned int binary_to_uint(const char *b)
+{
+    /*Checking to see if pointer is not null*/
+    int i = 0, power = 1;
+    unsigned int sum = 0, error = 0;
+
+    while(b[i] != '\0')
+    {
+        if(b[i] != '0' && b[i] != '1')
+            return (error);
+        i++;
+
+    }
+    i--;
+
+    while(i >= 0)
+    {
+        if (b[i] == '1')
+            sum += power;
+        power *= 2;
+        i--;
+    }
+
+    return (sum);
+}
