b37f41c29617c0e2a5401ff63b0b38f41a79fa9f
Michael Sarkodie <sarkodiemichael399@gmail.com>
2022-08-31 12:45:15 +0000

    Made in Betty style code

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,27 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
+/**
+ * binary_to_uint - converts bianary to decimal
+ *@b: The pointer to the binary
+ * Return: the decimal result
+ */
+
 unsigned int binary_to_uint(const char *b)
 {
-    /*Checking to see if pointer is not null*/
-    int i = 0, power = 1;
-    unsigned int sum = 0, error = 0;
+	 /*Checking to see if pointer is not null*/
+	int i = 0, power = 1;
+	unsigned int sum = 0, error = 0;
 
-    while(b[i] != '\0')
-    {
-        if(b[i] != '0' && b[i] != '1')
-            return (error);
-        i++;
+	if (b == NULL)
+	{
+		return (error);
+	}
+	while (b[i] != '\0')
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (error);
+	i++;
+	}
+	i--;
 
-    }
-    i--;
+	while (i >= 0)
+	{
+		if (b[i] == '1')
+		sum += power;
+		power *= 2;
+		i--;
+	}
 
-    while(i >= 0)
-    {
-        if (b[i] == '1')
-            sum += power;
-        power *= 2;
-        i--;
-    }
-
-    return (sum);
+	return (sum);
 }
